50101a3df7055bf75facf203e8f5d07a923bf11e
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-11-09 12:45:32 -0500

    [WORKING] Absolute routes working without errors

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="vite/client" />
+declare module "@components/*";
+declare module "@pages/*";
+declare module "@css/*";
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@css': path.resolve(__dirname, './src/css')
-    }
-  }
-})
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(path.join(__dirname, "./src")),
+      },
+      {
+        find: "@pages",
+        replacement: path.resolve(path.join(__dirname, "./src/pages")),
+      },
+      {
+        find: "@css",
+        replacement: path.resolve(path.join(__dirname, "./src/css")),
+      },
+      {
+        find: "@components",
+        replacement: path.resolve(path.join(__dirname, "./src/components")),
+      },
+    ],
+  },
+});
