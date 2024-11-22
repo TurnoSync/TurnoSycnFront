ce430fd1f070d3a92eea4af11aee3587e5709188
Andrés Felipe López Becerra <andreslopzi@hotmail.com>
2024-11-21 22:39:32 -0500

    feat: add not found page and update routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@pages/general/home.jsx";
 import Professional from "@pages/professional/professional.jsx";
 import Customer from "@pages/customer/customer.jsx";
+import NotFound from "@pages/general/notFound";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
-          <Route
-            path="/professional"
-            element={<Professional></Professional>}
-          ></Route>
-          <Route path="/customer" element={<Customer></Customer>}></Route>
+          <Route path="/" Component={Home} />
+          <Route path="/professional" Component={Professional} />
+          <Route path="/customer" Component={Customer} />
+          <Route path="*" Component={NotFound} />
         </Routes>
       </BrowserRouter>
     </>
