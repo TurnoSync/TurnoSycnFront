4d067036ceca4c5cb525f9e82c224eb292aca75e
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-11-26 11:43:29 -0500

    test

--- src/infrastructure/api/serviceAPI.ts
+++ src/infrastructure/api/serviceAPI.ts
@@ -4,6 +4,7 @@ import { Service } from "../../domain/models/service";
 import { ServiceRepository } from "../../domain/repositories/serviceRepository";
 import config from "../config/config";
 
+console.log(config.endpoint_services);
 export class ServiceAPI implements ServiceRepository {
   private readonly endpoint = config.endpoint_services;
 
